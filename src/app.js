8db76afe6c0ebbd501a7268ceceefbb99dd287ae
Andres Villar <andresvillarx@gmail.com>
2016-03-30 17:00:19 -0400

    some changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ var HelloWorldLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
         //Obteniendo el tamaño de la pantalla
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprFondo:null,
+    sprConejo:null,
+    bomb: [],
+    carrots: [],
+    
+    
+    function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+    ctor:function () {
+        this._super();
+        //Obteniendo el tamaño de la pantalla
         var size = cc.winSize;
 
         //posicionando la imagen de fondo
@@ -16,6 +31,24 @@ var HelloWorldLayer = cc.Layer.extend({
         this.sprConejo = new cc.Sprite(res.conejo_png);
         this.sprConejo.setPosition(size.width / 2,size.height * 0.15);
         this.addChild(this.sprConejo, 1);
+    
+    
+        //Handles the movement with keyboard input ~
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function(keyCode, event){
+                
+                var gam = event.getCurrentTarget();
+                //right
+                if(keyCode === 39 && gam.Bunny.x < 670){    
+                    gam.Bunny.x += 10;
+                }
+                //left
+                if(keyCode === 37 && gam.Bunny.x > 270){      
+                    gam.Bunny.x -= 10;
+                }
+            }
+        }, this);
 
 
         return true;
